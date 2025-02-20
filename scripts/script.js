7698c96124321047e500ae208f766f088e86bd38
David Bissaco da Silva <davidbissacodasilva@gmail.com>
2025-02-20 20:48:50 -0300

    button back-to-top

---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+window.onscroll = function () {
+    const backToTopButton = document.getElementById('back-to-top');
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        backToTopButton.style.display = 'block';
+    } else {
+        backToTopButton.style.display = 'none';
+    }
+};
+document.getElementById('back-to-top').onclick = function () {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+};
